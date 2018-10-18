fa567ba3d4668c4b071c89d2985b5406f809825c
Ijeri Omitogun <32554410+ijeriomit@users.noreply.github.com>
2018-10-18 18:03:38 -0500

    new file

--- src/ImageIO.py
+++ src/ImageIO.py
@@ -1,8 +1,25 @@
+from PIL import Image
+
+
 class ImageIO:
+    valid_types = [".jpg", ".gif", ".png", ".tga"]
 
     @classmethod
-    def loadImages(cls):
-        imglist = list()
-        for i in range(0,6):
-            imglist.append(i)
-        return imglist
+    def loadImages(cls, side, filetype):
+
+        if not cls.checkValidImageFormat(filetype):
+            print("INVALID FILE TYPE")
+        path = "/Ijeri/Documents/PycharmProjects/Rubix-Cube/Cube_1/Side_"+side+filetype
+        img = Image.open(path,'rb')
+        return img
+
+    @classmethod
+    def checkValidImageFormat(cls,filetype):
+
+        if filetype in cls.valid_types:
+            return True
+        return False
+
+
+
+
--- test src/ImageIOTest.py
+++ test src/ImageIOTest.py
@@ -4,10 +4,14 @@ from src.ImageIO import ImageIO
 
 class ImageIOTest(unittest.TestCase):
 
-
     def test_loadImage(self):
-        self.assertEqual(len(ImageIO.loadImages()), 6)
+        self.assertNotEqual(ImageIO.loadImages(), n)
 
+    def test_FileTypeIsValid(self):
+        self.assertTrue(ImageIO.checkValidImageFormat(".jpg"))
+
+    def test_FileTypoisInvalid(self):
+        self.assertFalse((ImageIO.checkValidImageFormat("jepeo")))
 
 if __name__ == '__main__':
     unittest.main()

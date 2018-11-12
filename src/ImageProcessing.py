854ddc51e195b2ce2c9834cad99c765204f077d0
ijeriomit <iiomitogun@uh.edu>
2018-11-12 16:38:45 -0600

    Implementing color identification

--- src/ImageProcessing.py
+++ src/ImageProcessing.py
@@ -1,25 +1,19 @@
 import numpy as np
 import cv2
 class ImageProcessing:
-    #redboundary =
-    #blueboundary =
-    #yellowboundary =
-    #whiteboundary =
-    #orangeboundary =
-    #greenboundary =
     @classmethod
     def splitImage(cls,image):
         imgRegions = dict()
         height,width = np.size(image,0),np.size(image,1)
-        print(height,width)
+       # print(height,width)
         for i in range(0,3):
             yupperbound = int((i+1) * (height / 3))
             ylowerbound = int(yupperbound - (height / 3))
-            print("YL: ", ylowerbound, "YU: ", yupperbound)
+            #print("YL: ", ylowerbound, "YU: ", yupperbound)
             for j in range(0,3):
                 xupperbound = int((j+1)*(width/3))
                 xlowerbound = int(xupperbound-(width/3))
-                print("XL: ", xlowerbound, "XU: ", xupperbound)
+                #print("XL: ", xlowerbound, "XU: ", xupperbound)
                 imgRegions[(i,j)] = image[ylowerbound:yupperbound,xlowerbound:xupperbound]
         cv2.imshow("Cube Section",imgRegions.get((0,0)))
         cv2.waitKey(0)
@@ -29,11 +23,78 @@ class ImageProcessing:
     @classmethod
     def isBlue(cls, colorstats):
         b,g,r = colorstats
-        return ((b > r) and (r == g) or (b == g) and (b > r) or (b > g) and (g > r) or (b > r) and (r > g))
+        return (b > r) and (r == g) or (b == g) and (b > r) or (b > g) and (g > r) or (b > r) and (r > g)
+
+    @classmethod
+    def isGreen(cls, colorstats):
+        b,g,r = colorstats
+        return (g > r) and (r > b) and ((g - r) >= (.25 * r)) or (g > r) and (r == b) or (g > b) and (b > g)
+
+    @classmethod
+    def isRed(cls, colorstats):
+        b, g, r = colorstats
+        return (r > g) and (g == b) or (r > g) and (g > b) and ((g - b) < (.25*b)) or (r > b) and (b > g) or (r == b) and (b > g)
+
+    @classmethod
+    def isYellow(cls, colorstats): #not done
+        b, g, r = colorstats
+        return (g > r) and (r > b) and ((g - r) < (.25 * r)) or (g == r) and (r > b) or (r > g) and (g > b) and ((r - g) < (.25*g)) and ((g - b) >= (.25*b))
+
+    @classmethod
+    def isOrange(cls, colorstats):
+        b, g, r = colorstats
+        return (r > g) and (g > b) and ((r - g) >= (.25*g)) and ((g - b) >= (.25*b))
+
+    @classmethod
+    def isWhite(cls, colorstats): #not done
+        b, g, r = colorstats
+        return (((r-g) <= (.05*g)) and ((r-b) <= (.05*b)) and ((b-g) <= (.05*g))) and (r+b+g) >= 550
 
     @classmethod
     def calulatecolorstats(cls, color):
-        sum = color[0]+color[1]+color[2]
-        return color[0]/sum, color[1]/sum,color[2]/sum
+        sum = color[0] + color[1] + color[2]
+        return color[0] / sum, color[1] / sum, color[2] / sum
+
+    @classmethod
+    def calculateHue(cls, pixel):
+        b, g, r = pixel
+        if r == g == b:
+            return None
+        if cls.isWhite(pixel):
+            return -1
+        elif (r >= g) and (g >= b):
+            hue = 60 * ((g - b)/(r - b))
+        elif (g > r) and (r >= b):
+            hue = 60 * (2 - ((r - b)/(g - b)))
+        elif (g >= b) and (b > r):
+            hue = 60 * (2 + ((b - r)/(g - r)))
+        elif (b > g) and (g > r):
+            hue = 60 * (4 - ((g - r)/(b - r)))
+        elif (b > r) and (r >= g):
+            hue = 60 * (4 + ((r - g)/(b - g)))
+        elif (r >= b) and (b > g):
+            hue = 60 * (6 - ((b - g)/(r - g)))
+        return hue
+
+    @classmethod
+    def chooseColor(cls,hue):
+        if hue is None:
+            return "BlANK"
+        elif hue == -1:
+            return "WHITE"
+        elif hue >= 320 or hue <= 10:
+            return "RED"
+        elif 10 < hue <= 45:
+            return "ORANGE"
+        elif 45 < hue <= 60:
+            return "YELLOW"
+        elif 60 < hue <= 169:
+            return "GREEN"
+        elif 169 < hue <= 320:
+            return "BLUE"
+        else:
+            return "BlANK"
+
+
 
 
--- test src/ImageProcessingTest.py
+++ test src/ImageProcessingTest.py
@@ -15,14 +15,72 @@ class MyTestCase(unittest.TestCase):
             for i in seen:
                 self.assertFalse(np.array_equal(x,i))
             seen.append(x)
-    def test_colorisred(self):
+
+    def test_colorisRedwithIsRed(self):
         self.assertTrue(imgPro.isRed((0, 0, 255)))
 
-    def test_colorisblue(self):
+    def test_colorisRedwithChooseColor(self):
+        self.assertEqual("RED", imgPro.chooseColor(imgPro.calculateHue((0, 0, 255))))
+
+    def test_colorisBluewithIsBlue(self):
         self.assertTrue(imgPro.isBlue((255, 0, 0)))
 
-    def test_colorsisgreen(self):
+    def test_colorisBluewithChooseColor(self):
+        self.assertEqual(imgPro.chooseColor(imgPro.calculateHue((255, 0, 0))), "BLUE")
+
+    def test_colorisGreenwithIsGreen(self):
         self.assertTrue(imgPro.isGreen((0, 255, 0)))
+
+    def test_colorisGreenwithChooseColor(self):
+        self.assertEqual(imgPro.chooseColor(imgPro.calculateHue((0, 255, 0))), "GREEN")
+
+    def test_colorisYellowwithIsYellow(self):
+        self.assertTrue(imgPro.isYellow((0, 255, 255)))
+
+    def test_colorisYellowwithChooseColor(self):
+        self.assertEqual(imgPro.chooseColor(imgPro.calculateHue((0, 255, 255))), "YELLOW")
+
+    def test_colorisOrangewithIsOrange(self):
+        self.assertTrue(imgPro.isOrange((0, 127, 255)))
+
+    def test_colorisOrangewithChooseColor(self):
+        self.assertEqual(imgPro.chooseColor(imgPro.calculateHue((0, 127, 255))), "ORANGE")
+
+
+
+    def test_coloriswhite(self):
+       self.assertTrue(imgPro.isWhite((255, 255, 255)))
+
+    def test_graycolorwithChooseColor(self):
+        self.assertEqual("WHITE", imgPro.chooseColor(imgPro.calculateHue((0, 0, 0))))
+
+
+
+    def test_greenishblueisbluewithIsBlue(self):
+       self.assertTrue((imgPro.isBlue((139, 139, 0))))
+
+    def test_greenishblueisbluewithChooseColor(self):
+        self.assertEqual("BLUE", imgPro.chooseColor(imgPro.calculateHue((139, 139, 0))))
+
+#challenge
+    def test_orangishredisredwithIsRed(self):
+        self.assertTrue((imgPro.isRed((92, 115, 232))))
+
+    def test_orangishredisredwithChooseColor(self): #winner
+        self.assertEqual("RED", imgPro.chooseColor(imgPro.calculateHue((92, 115, 232))))
+
+    def test_orangeishyellowisyellowwithIsYellow(self):
+        self.assertTrue((imgPro.isYellow((0, 198, 255))))
+
+    def test_orangeishyellowisyellowwithChooseColor(self): #winner
+        self.assertEqual("YELLOW", imgPro.chooseColor(imgPro.calculateHue((0, 198, 255))))
+
+
+
+
+
+   # def test_purplishblueisblue(self):
+    #    self.assertTrue((imgPro.isBlue((255, 0, 143))))
 
     def test_calculatecolorstats(self):
         self.assertEqual(imgPro.calulatecolorstats((255, 0, 0)), (1, 0, 0))
@@ -30,17 +88,6 @@ class MyTestCase(unittest.TestCase):
     def test_calculatestatswithfloatingpointdecimals(self):
         stats = imgPro.calulatecolorstats((251, 42, 78))
         self.assertEqual(stats, (0.6765498652291105, 0.11320754716981132,0.21024258760107817))
-
-    def test_isbluewithcalculatestats(self):
-        self.assertTrue(imgPro.isBlue(imgPro.calulatecolorstats((255, 0, 0))))
-
-    def test_greenishblueisblue(self):
-        self.assertTrue((imgPro.isBlue(imgPro.calulatecolorstats((139, 139, 0)))))
-
-    def test_purplishblueisblue(self):
-        self.assertTrue((imgPro.isBlue(imgPro.calulatecolorstats((255, 0, 143)))))
-
-
 
 if __name__ == '__main__':
     unittest.main()
